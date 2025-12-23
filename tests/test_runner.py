f7507dc46c83196d2237b27b2a37507532b0f68e
sarkar94s <soham4894@gmail.com>
2025-12-22 23:01:47 -0700

    Update workflow

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -41,7 +41,7 @@ class TestRunner:
         self,
         test_definition: Dict,
         reset_app_before: bool = True,
-        use_hardcoded_steps: bool = True
+        use_hardcoded_steps: bool = False
     ) -> Dict[str, Any]:
         """
         Run a single test
